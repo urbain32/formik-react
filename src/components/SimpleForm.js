b2f64f5e7f0b9b02cc666451f536412b52b55a47
urbain32 <urbainq32@gmail.com>
2022-12-01 11:27:31 +0200

    creating a validate form in formik

--- src/components/SimpleForm.js
+++ src/components/SimpleForm.js
@@ -1,17 +1,38 @@
 import { Box, Button, TextField, Typography } from '@mui/material';
 import React from 'react';
 import { useFormik } from 'formik';
+const initialValues = {
+  name: 'Urban',
+  email: '',
+  channel: '',
+};
+const onSubmit = (values) => {
+  console.log('Formik values', values);
+};
+const validate = (values) => {
+  let errors = {};
+  const regex = !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email);
+  if (!values.name) {
+    errors.name = 'Required';
+  }
+  if (!values.email) {
+    errors.email = 'Required';
+  } else if (regex) {
+    errors.email = 'Invalid email format';
+  }
+  if (!values.channel) {
+    errors.channel = 'Required';
+  }
+  return errors;
+};
+
 const SimpleForm = () => {
   const formik = useFormik({
-    initialValues: {
-      name: '',
-      email: '',
-      channel: '',
-    },
-    onSubmit: (values) => {
-      console.log('Formik values', values);
-    },
+    initialValues,
+    onSubmit,
+    validate,
   });
+  console.log('Formik errors', formik.errors);
   return (
     <Box sx={{ border: 1, borderRadius: 4, p: 2, m: 2 }}>
       <Typography variant='h6' align='center'>
@@ -26,35 +47,47 @@ const SimpleForm = () => {
         noValidate
         autoComplete='off'
       >
-        <div>
+        <div className='form-control'>
           <TextField
             fullWidth
             id='name'
+            name='name'
             label='Name'
             variant='outlined'
             onChange={formik.handleChange}
             value={formik.values.name}
           />
+          {formik.errors.name ? (
+            <div className='error'>{formik.errors.name}</div>
+          ) : null}
         </div>
-        <div>
+        <div className='form-control'>
           <TextField
             fullWidth
             id='email'
+            name='email'
             label='Email'
             variant='outlined'
             onChange={formik.handleChange}
             value={formik.values.email}
           />
+          {formik.errors.email ? (
+            <div className='error'>{formik.errors.email}</div>
+          ) : null}
         </div>
-        <div>
+        <div className='form-control'>
           <TextField
             fullWidth
             id='channel'
+            name='channel'
             label='Channel'
             variant='outlined'
             onChange={formik.handleChange}
             value={formik.values.channel}
           />
+          {formik.errors.channel ? (
+            <div className='error'>{formik.errors.channel}</div>
+          ) : null}
         </div>
         <div>
           <Button fullWidth type='submit' variant='contained' color='success'>
